5c86762b118515ef96e6ec01d9ad415bdc2bc3b1
zubair-appiskey <zubair.moosani@appiskey.com>
2023-01-15 20:20:26 +0500

    create typescript currency converter cli app with inquirer

---
+++ index.js
@@ -0,0 +1,45 @@
+import inquirer from "inquirer";
+const converstion = {
+    USD: {
+        USD: 1,
+        AED: 3.67,
+        PKR: 228.03,
+    },
+    AED: {
+        USD: 0.27,
+        AED: 1,
+        PKR: 62.08,
+    },
+    PKR: {
+        USD: 0.0044,
+        AED: 0.016,
+        PKR: 1,
+    },
+};
+const answers = await inquirer.prompt([
+    {
+        type: "list",
+        name: "from",
+        choices: ["USD", "AED", "PKR"],
+        message: "Select your currency: ",
+    },
+    {
+        type: "list",
+        name: "to",
+        choices: ["USD", "AED", "PKR"],
+        message: "Select your convertion currency: ",
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter your amount",
+    },
+]);
+const { from, to, amount } = answers;
+if (from && to && amount) {
+    const result = converstion[from][to] * amount;
+    console.log(result);
+}
+else {
+    console.log("Invalid input");
+}
---
+++ index.ts
@@ -0,0 +1,53 @@
+import inquirer from "inquirer";
+
+type Answers = {
+  from: "USD" | "AED" | "PKR";
+  to: "USD" | "AED" | "PKR";
+  amount: number;
+};
+
+const converstion = {
+  USD: {
+    USD: 1,
+    AED: 3.67,
+    PKR: 228.03,
+  },
+  AED: {
+    USD: 0.27,
+    AED: 1,
+    PKR: 62.08,
+  },
+  PKR: {
+    USD: 0.0044,
+    AED: 0.016,
+    PKR: 1,
+  },
+};
+const answers: Answers = await inquirer.prompt([
+  {
+    type: "list",
+    name: "from",
+    choices: ["USD", "AED", "PKR"],
+    message: "Select your currency: ",
+  },
+  {
+    type: "list",
+    name: "to",
+    choices: ["USD", "AED", "PKR"],
+    message: "Select your convertion currency: ",
+  },
+  {
+    type: "number",
+    name: "amount",
+    message: "Enter your amount",
+  },
+]);
+
+const { from, to, amount } = answers;
+
+if (from && to && amount) {
+  const result = converstion[from][to] * amount;
+  console.log(result);
+} else {
+  console.log("Invalid input");
+}
